9eeb106446ef98ef185300d66248c0fef321ceed
ruslanlap <lapin@ucu.edu.ua>
2026-09-25 11:17:04 +0000

    fix: restore System.Text.Json using for JsonException

--- Definition/Community.PowerToys.Run.Plugin.Definition/Main.cs
+++ Definition/Community.PowerToys.Run.Plugin.Definition/Main.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Reflection;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
